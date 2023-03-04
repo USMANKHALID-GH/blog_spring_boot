479c079661a2a440ce33c16c989bafc98d3adecc
merve824 <loftyusman@gmail.com>
2023-03-05 02:45:09 +0300

    general exception

--- src/main/java/org/usman/blog_spring_boot/advice/GeneralException.java
+++ src/main/java/org/usman/blog_spring_boot/advice/GeneralException.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.usman.blog_spring_boot.Error.IdNotFoundException;
+import org.usman.blog_spring_boot.Error.PhraseNotFoundEXception;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,8 +16,8 @@ public class GeneralException {
 
 
 
-
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+// entity validation error
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST,code = HttpStatus.INTERNAL_SERVER_ERROR)
    @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> GeneralExeption(MethodArgumentNotValidException ex){
         Map<String ,String> map= new HashMap<>();
@@ -27,11 +28,23 @@ public class GeneralException {
 
    }
 
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+//   id not found
+    @ResponseStatus(value = HttpStatus.NOT_FOUND,code = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(IdNotFoundException.class)
     public Map<String, String> notFound(IdNotFoundException ex){
         Map<String ,String> map= new HashMap<>();
-       map.put("ErrorMesage",ex.getMessage());
+       map.put("IdError",ex.getMessage());
+
+        return map;
+
+    }
+
+//    phrase returning empty
+    @ResponseStatus(value = HttpStatus.NOT_FOUND ,code = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(PhraseNotFoundEXception.class)
+    public Map<String, String> PhraseNotFound(PhraseNotFoundEXception ex){
+        Map<String ,String> map= new HashMap<>();
+        map.put("phraseError",ex.getMessage());
 
         return map;
 
