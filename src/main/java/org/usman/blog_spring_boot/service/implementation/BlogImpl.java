8e23c9aca67f51613ca49042a001453bdb31e8ac
merve824 <loftyusman@gmail.com>
2023-03-05 02:47:59 +0300

    service class

--- src/main/java/org/usman/blog_spring_boot/service/implementation/BlogImpl.java
+++ src/main/java/org/usman/blog_spring_boot/service/implementation/BlogImpl.java
@@ -3,6 +3,7 @@ package org.usman.blog_spring_boot.service.implementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.usman.blog_spring_boot.Error.IdNotFoundException;
+import org.usman.blog_spring_boot.Error.PhraseNotFoundEXception;
 import org.usman.blog_spring_boot.dto.BlogDto;
 import org.usman.blog_spring_boot.dto.BlogGeneralDto;
 import org.usman.blog_spring_boot.dto.CatDto;
@@ -13,11 +14,11 @@ import org.usman.blog_spring_boot.service.serviceInterface.BlogInt;
 import org.usman.blog_spring_boot.service.serviceInterface.CatInter;
 import org.usman.blog_spring_boot.utility.Blog;
 import org.usman.blog_spring_boot.utility.Cat;
-
-import java.awt.print.Pageable;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+
+
 
 @Service
 public class BlogImpl implements BlogInt, CatInter {
@@ -32,13 +33,16 @@ public class BlogImpl implements BlogInt, CatInter {
     @Autowired
     private MapperDto mapperDto;
 
+
+//    saving category to database
     @Override
     public Cat saveCat(Cat cat) {
-        System.out.println("====================================================");
-        System.out.println(cat.getCategory());
+
+        cat.setCategory(cat.getCategory().toLowerCase());
         return  catRepository.save(cat);
     }
 
+//    find category by id
     @Override
     public CatDto findCat(Long id) {
 
@@ -50,19 +54,19 @@ public class BlogImpl implements BlogInt, CatInter {
 
     }
 
+//    updating category using cat id
     @Override
     public String updateCat(Long id, CatDto catDto) {
         Optional<Cat> cat=catRepository.findById(id);
         if(cat.isPresent()){
             Cat cat2=cat.map(cat1 -> {
                 if(catDto.getCategory()!="" || catDto.getCategory()!=null){
-            cat1.setCategory(catDto.getCategory());}
+            cat1.setCategory(catDto.getCategory().toLowerCase());}
 
             return cat1;
 
 
             }).get();
-        System.out.println(cat2.getCategory());
         catRepository.save(cat2);
 
 
@@ -71,15 +75,18 @@ public class BlogImpl implements BlogInt, CatInter {
             throw new IdNotFoundException("there is no such Id "+id);
     }
 
+
+//    find by using cat name
     @Override
     public CatDto findByName(String name) {
 
-        Cat cat=catRepository.findCatByCategory(name);
-        System.out.println(cat.getCategory()+"-------------------------------");
+        Cat cat=catRepository.findCatByCategory(name.toLowerCase());
+
         return mapperDto.entityToCatDto(cat);
 
     }
 
+//    delete category using category id
     @Override
     public String deleteCat(Long id) {
         if(catRepository.findById(id).isPresent()){
@@ -90,32 +97,57 @@ public class BlogImpl implements BlogInt, CatInter {
             throw new IdNotFoundException("there is no such Id "+id);
     }
 
+
+//    get all category
     @Override
     public List<CatDto> getAll() {
         return mapperDto.entityCatToDto(catRepository.findAll());
     }
 
+//    searching for a word or phrase in blog content
     @Override
     public List<BlogGeneralDto> searchInCentent(String string) {
 
-        Optional<List<Blog>> blogs=blogRepository.findBySearchParameter(string);
-        System.out.println(blogs.get()+"000000000000000000000000000000000000000000000000000");
-        List<BlogGeneralDto> blogGeneralDtos=mapperDto.entityToDto(blogs.get());
-        return blogGeneralDtos;
+        List<Blog> blogs= blogRepository.findByContentContaining(string.toLowerCase().trim());
+        System.out.println("......................."+blogs.isEmpty());
+        if(!blogs.isEmpty()) {
 
+            List<BlogGeneralDto> blogGeneralDtos = mapperDto.entityToDto(blogs);
+            return blogGeneralDtos;
+        }
+        else
+            throw new PhraseNotFoundEXception("there is no such phrase in our database "+string);
 
     }
 
+
+//    searching for full title of part of title in the blog table
+    @Override
+    public List<BlogGeneralDto> searchInTitle(String string) {
+        List<Blog> blogs =blogRepository.findByTitleContaining(string.toLowerCase().trim());
+        if(!blogs.isEmpty()){
+            return mapperDto.entityToDto(blogs);
+        }
+        else
+            throw  new PhraseNotFoundEXception("there is not such phrase in our database"+string);
+
+    }
+
+
+//    saving a blog
     @Override
     public BlogGeneralDto saveBlog(BlogGeneralDto blogGeneralDto, Long id) {
         Optional<Cat> cat =catRepository.findById(id);
-        System.out.println("...................????????????"+cat.get());
+        System.out.println(cat.isPresent()+"ooooooooooooooooooooooooooooooooooooooooo");
         if(cat.isPresent()){
 
             Blog blog =mapperDto.dtoToEntity(blogGeneralDto);
+            blog.setContent(blog.getContent().toLowerCase());
+            blog.setTitle(blog.getTitle());
             blog.setDateCreated(LocalDate.now());
+
             blog.setCat(cat.get());
-            System.out.println("................"+blog.getDateCreated());
+
             blogRepository.save(blog);
             return  mapperDto.entityToDto(blog);
         }
@@ -123,6 +155,7 @@ public class BlogImpl implements BlogInt, CatInter {
             throw new IdNotFoundException("there is no such Id "+id);
     }
 
+//    shall all blogs
     @Override
     public List<BlogGeneralDto> showAll() {
         System.out.println("Service Class!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
@@ -132,6 +165,7 @@ public class BlogImpl implements BlogInt, CatInter {
         return blogDtos;
     }
 
+//    showing blog using blog id
     @Override
     public BlogGeneralDto blogFindById(Long id) {
         Optional<Blog> blog=blogRepository.findById(id);
@@ -142,6 +176,7 @@ public class BlogImpl implements BlogInt, CatInter {
 
     }
 
+//    shall all blog but has an error yet to be solved
     @Override
     public List<BlogDto> findAllBlod() {
         List<Blog> blogs=blogRepository.findAll();
@@ -149,6 +184,7 @@ public class BlogImpl implements BlogInt, CatInter {
         return blogDtos;
     }
 
+//updating a blog
     @Override
     public String updateBlog(Long id,BlogGeneralDto blogGeneralDto) {
         Optional<Blog> cat=blogRepository.findById(id);
@@ -157,11 +193,11 @@ public class BlogImpl implements BlogInt, CatInter {
 
         Blog blog1=cat.map(blog -> {
             if(blogGeneralDto.getContent()!="" || blogGeneralDto.getContent()!=""){
-            blog.setContent(blogGeneralDto.getContent());}
+            blog.setContent(blogGeneralDto.getContent().toLowerCase());}
             if(blogGeneralDto.getTitle()!="" || blogGeneralDto.getContent()!=null){
-            blog.setTitle(blogGeneralDto.getTitle());}
+            blog.setTitle(blogGeneralDto.getTitle().toLowerCase());}
             if(blogGeneralDto.getImage()!=""||blogGeneralDto.getImage()!=null){
-                blog.setImage(blogGeneralDto.getImage());
+                blog.setImage(blogGeneralDto.getImage().toLowerCase());
             }
 
             return blog;
@@ -174,6 +210,7 @@ public class BlogImpl implements BlogInt, CatInter {
         else throw new IdNotFoundException("there is no such Id "+id);
     }
 
+//    deleting a blog by id
     @Override
     public String deleteBlog(Long id) {
 
