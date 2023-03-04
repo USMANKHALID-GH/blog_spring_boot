8b9f3cc5fe98eb22a3fecb32258cd58d20072d1a
merve824 <loftyusman@gmail.com>
2023-03-05 02:47:21 +0300

    blog controller

--- src/main/java/org/usman/blog_spring_boot/controller/BlogController.java
+++ src/main/java/org/usman/blog_spring_boot/controller/BlogController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.relational.core.sql.In;
 import org.springframework.web.bind.annotation.*;
 import org.usman.blog_spring_boot.Error.IdNotFoundException;
+import org.usman.blog_spring_boot.Error.PhraseNotFoundEXception;
 import org.usman.blog_spring_boot.dto.BlogDto;
 import org.usman.blog_spring_boot.dto.BlogGeneralDto;
 import org.usman.blog_spring_boot.dto.CatDto;
@@ -20,35 +21,41 @@ public class BlogController {
 
     @Autowired
     private BlogImpl blog;
-
+//main page
     @GetMapping("/")
     public  String hello(){
         return  "hello!!!!!!!!!!!";
     }
 
+//    saving blog APi
     @PostMapping("/save/{id}")
     public BlogGeneralDto createBlog(@RequestBody @Valid BlogGeneralDto blogGeneralDto,
                                      @PathVariable("id") Long id)throws IdNotFoundException{
         System.out.println(".........................."+id);
-        System.out.println("///////////////////////////////////"+blogGeneralDto.toString());
+
         return blog.saveBlog(blogGeneralDto,id);
     }
 
+//    showing all blog api
     @GetMapping("/all")
     public List<BlogGeneralDto> showAllBlog(){
         return blog.showAll();
     }
 
+//    get blog by id api
     @GetMapping("/find/{id}")
     public BlogGeneralDto findBlogById(@PathVariable("id") Long id)throws IdNotFoundException{
         return blog.blogFindById(id);
     }
 
+//    and error with this api getting all blog
     @GetMapping("/allblog")
     public List<BlogDto> AllBlog(){
         return blog.findAllBlod();
     }
 
+
+//    updating blog api
     @PutMapping("/update/{id}")
     public  String updatecat(@PathVariable("id") Long id,
                              @RequestBody @Valid BlogGeneralDto catDto)throws IdNotFoundException{
@@ -58,16 +65,24 @@ public class BlogController {
 
     }
 
+//    deleting blog api
     @DeleteMapping("/delete/{id}")
     public String deleteByCatId(@PathVariable("id") Long name) throws IdNotFoundException {
         return blog.deleteBlog(name);
     }
 
+//    searching in centent api
     @GetMapping("/search/{sentence}")
-    public  List<BlogGeneralDto> searchForPhrase(@PathVariable("sentence") String sentence){
+    public  List<BlogGeneralDto> searchForPhrase(@PathVariable("sentence") String sentence) throws PhraseNotFoundEXception {
         System.out.println("/////////////////////////////////////////////"+sentence);
         return  blog.searchInCentent(sentence);
     }
+
+//searching for a title api
+        @GetMapping("findby/{string}")
+        public List<BlogGeneralDto> SearchTitle(@PathVariable("string") String string) throws  PhraseNotFoundEXception{
+         return  blog.searchInTitle(string);
+        }
 
 
 
