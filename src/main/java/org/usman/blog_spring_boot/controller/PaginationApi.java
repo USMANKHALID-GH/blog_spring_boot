2a27a5f52073eded837fc92f46ec43877513d1b6
merve824 <loftyusman@gmail.com>
2023-03-06 13:22:25 +0300

    pagination

---
+++ src/main/java/org/usman/blog_spring_boot/controller/PaginationApi.java
@@ -0,0 +1,47 @@
+package org.usman.blog_spring_boot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.usman.blog_spring_boot.dto.BlogGeneralDto;
+import org.usman.blog_spring_boot.service.implementation.BlogImpl;
+import org.usman.blog_spring_boot.service.implementation.Pagination_Sorting;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1")
+public class PaginationApi {
+    @Autowired
+    private Pagination_Sorting pagination_sorting;
+
+    //   pagination and sorting
+
+    @GetMapping(path = {"/get/{offset}/{pageSize}/{field}/{order}","/get"
+    ,"/get/{offset}", "/get/{offset}/{pageSize}","/get/{offset}/{pageSize}/{field}"})
+    public List<BlogGeneralDto> getAll(@PathVariable(value = "offset", required = false) Integer offset,
+                                       @PathVariable(value = "pageSize",required = false)  Integer pageSize,
+                                       @PathVariable(value = "field",required = false )String field,
+                                       @PathVariable(value = "order",required = false) String order
+                                       ){
+
+        if(order==null){
+            return  pagination_sorting.getAll(offset,pageSize,field,"desc");
+        }
+        else
+            return  pagination_sorting.getAll(offset,pageSize,field,order);
+
+    }
+
+    @GetMapping(path = {"/get1","/get1/{id}"})
+    public  String get(@PathVariable(value = "id", required = false) Integer id){
+        if(id==null){
+        return "just testing  ";}
+        else
+            return id+" yeahhhh";
+    }
+
+
+}
---
+++ src/main/java/org/usman/blog_spring_boot/service/implementation/Pagination_Sorting.java
@@ -0,0 +1,60 @@
+package org.usman.blog_spring_boot.service.implementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.usman.blog_spring_boot.dto.BlogGeneralDto;
+import org.usman.blog_spring_boot.mapperDto.MapperDto;
+import org.usman.blog_spring_boot.respository.BlogRepository;
+import org.usman.blog_spring_boot.utility.Blog;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class Pagination_Sorting {
+    @Autowired
+    private BlogRepository blogRepository;
+    @Autowired
+    private MapperDto mapperDto;
+
+
+    public List<BlogGeneralDto>  getAll(Integer offset,Integer pageSize,String field, String order){
+        Page<Blog> page=null;
+        if(offset==null){
+           offset=0;
+        }
+        if(pageSize==null){
+            pageSize=5;
+        }
+        if(field==null){
+            field="id";
+        }
+
+        if(field==null){
+
+            page=blogRepository.findAll(PageRequest.of(offset,pageSize).withSort(Sort.by(field)));
+        }
+
+
+        else if(order.equalsIgnoreCase("asc")){
+
+            page=blogRepository.findAll(PageRequest.of(offset,pageSize).withSort(Sort.by(Sort.Direction.ASC,field)));
+
+        }
+        else {
+            page=blogRepository.findAll(PageRequest.of(offset,pageSize).withSort(Sort.by(Sort.Direction.DESC,field)));
+
+        }
+
+
+
+
+        return  mapperDto.entityToDto(page.stream().collect(Collectors.toList()));
+    }
+
+
+
+}
