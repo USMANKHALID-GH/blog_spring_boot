bcf12405bef0c628d9c3af903b8bd36a51b17170
merve824 <loftyusman@gmail.com>
2023-03-07 11:27:57 +0300

    repository

--- src/main/java/org/usman/blog_spring_boot/respository/BlogRepository.java
+++ src/main/java/org/usman/blog_spring_boot/respository/BlogRepository.java
@@ -4,12 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.usman.blog_spring_boot.dto.BlogGeneralDto;
-import org.usman.blog_spring_boot.utility.Blog;
+import org.usman.blog_spring_boot.model.Blog;
 
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BlogRepository  extends JpaRepository<Blog,Long> {
--- src/main/java/org/usman/blog_spring_boot/respository/CategoryRepository.java
+++ src/main/java/org/usman/blog_spring_boot/respository/CategoryRepository.java
@@ -2,10 +2,10 @@ package org.usman.blog_spring_boot.respository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.usman.blog_spring_boot.utility.Cat;
+import org.usman.blog_spring_boot.model.Cat;
 
 @Repository
-public interface CatRepository  extends JpaRepository<Cat,Long> {
+public interface CategoryRepository extends JpaRepository<Cat,Long> {
 
 //    searching in cat
     public  Cat findCatByCategory(String name);
